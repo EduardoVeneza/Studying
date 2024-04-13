d3ae6f6292b0d160685a46d2409201888d0551b1
Eduardo Silva Veneza <164424296+EduardoVeneza@users.noreply.github.com>
2024-04-13 12:33:43 -0300

    Create Q09 - UNEB.c

---
+++ Lista 3 Algoritmos Uneb/Q09 - UNEB.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <math.h>           // Bibliotecas utilizadas
+#include <locale.h>
+#include <string.h>
+
+int main (){
+    setlocale(LC_ALL, "Portuguese_Brazil");  //Set de linguagem
+
+    int i, j, comp, verif; // Variáveis utilizadas
+    verif = 0;
+    do
+    {   
+        int vet[10];
+
+        for (i = 0;  i < 10; i++){
+        printf("Digite o %dº elemento do vetor!\n", i + 1);     //Coleta de dados
+        scanf("%d", &vet[i]);
+        fflush(stdin);
+    }
+
+        printf("\n\nDigite o número que deseja conferir a existência no vetor!\n");  //Coleta de dados
+        scanf("%d", &comp);
+        fflush(stdin);
+
+        for (i = 0; i < 10; i++)
+        {
+            if(comp == vet[i]){
+                printf("\nSeu número (%d) foi encontrado na posição %d do vetor!\n", comp, i); //Verifica se o número existe no vetor
+
+            } 
+        }
+        
+        for (i = 0; i < 10; i++)
+        {
+            if(comp != vet[i]){     
+                verif++;            //Verifica se o número não existe no vetor
+            }
+        }
+        
+        if(verif == 10){
+            printf("\nO número (%d) não foi encontrado em nenhuna posição do vetor!\n", comp); //Retorna os valores
+        }
+
+        printf("\nDeseja rodar o código novamente?\nDigite 1 para SIM\nDigite 2 para NÃO\n");  //Loop de pergunta
+        scanf("%d", &j);
+        if(j == 1){
+            continue;
+        } else {
+            break;
+        }
+    } while (1);
+}
